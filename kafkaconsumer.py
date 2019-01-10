24dc88c0980573831a54af05d1b01052d49f2937
piseganesh123 <piseganesh123@gmail.com>
2019-01-10 15:04:00 +0530

    Commit Async

--- kafkaconsumer.py
+++ kafkaconsumer.py
@@ -3,4 +3,5 @@ consumer = KafkaConsumer('test', bootstrap_servers=['localhost:9092'])
 for message in consumer:
   print ("%s:%d:%d: key=%s value=%s" % (message.topic, message.partition, message.offset, message.key, message.value))
 
+consumer.commitAsync();
 consumer.close()
