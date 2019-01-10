cbb135c1575038dbf9cc50b0342ef13249ab3233
piseganesh123 <piseganesh123@gmail.com>
2019-01-10 12:21:32 +0530

    Created consumer file
    Created test consumer file

---
+++ kafkaconsumer.py
@@ -0,0 +1,6 @@
+from kafka import KafkaConsumer
+consumer = KafkaConsumer('test', bootstrap_servers=['localhost:9092'])
+for message in consumer:
+print ("%s:%d:%d: key=%s value=%s" % (message.topic, message.partition,
+                                          message.offset, message.key,
+                                          message.value))
