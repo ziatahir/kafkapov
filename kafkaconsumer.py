f2035608ceeeca1db46b5c107f0fffa9c1507a95
piseganesh123 <piseganesh123@gmail.com>
2019-01-14 12:22:12 +0530

    Moving call SAP code in consumer block

--- kafkaconsumer.py
+++ kafkaconsumer.py
@@ -6,9 +6,10 @@ for message in consumer:
     counter = counter + 1
     if counter == 1:
        print ("%s:%d:%d: key=%s value=%s" % (message.topic, message.partition, message.offset, message.key, message.value))
+       if message.value == "Create Charm"
+       print ("Call SAP to Create Charm")
+      
        break
 
 print (counter)
-if message.value == "Create Charm"
-    print ("Call SAP to Create Charm")
 consumer.close()
