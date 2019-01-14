969d9f600c5e673996996dd4334d68c39345512a
piseganesh123 <piseganesh123@gmail.com>
2019-01-14 12:02:56 +0530

    updated code for check

--- kafkaconsumer.py
+++ kafkaconsumer.py
@@ -1,8 +1,17 @@
 from kafka import KafkaConsumer
 consumer = KafkaConsumer('test', bootstrap_servers=['localhost:9092'])
-for message in consumer:
-  print ("%s:%d:%d: key=%s value=%s" % (message.topic, message.partition, message.offset, message.key, message.value))
 
-#print (consumer.count())
+#for message in consumer:
+#  print ("%s:%d:%d: key=%s value=%s" % (message.topic, message.partition, message.offset, message.key, message.value))
+
+counter = 0
+    for message in consumer:
+        counter = counter + 1
+        if counter == nbr_msg:
+            break
+
+    return counter 
+print (counter)
+
 consumer.commitAsync()
 consumer.close()
