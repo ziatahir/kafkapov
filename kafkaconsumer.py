2d18d195039b0c5d3e0db4f0cb2bafa572329f07
piseganesh123 <piseganesh123@gmail.com>
2019-01-14 12:24:32 +0530

    added : for if block calling SAP

--- kafkaconsumer.py
+++ kafkaconsumer.py
@@ -6,9 +6,8 @@ for message in consumer:
     counter = counter + 1
     if counter == 1:
        print ("%s:%d:%d: key=%s value=%s" % (message.topic, message.partition, message.offset, message.key, message.value))
-       if message.value == 'Create_Charm'
-       print ("Call SAP to Create Charm")
-      
+       if message.value == 'Create_Charm':
+            print ("Call SAP to Create Charm")
        break
 
 print (counter)
