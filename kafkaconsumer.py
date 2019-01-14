b4fd81d7c768b2715ebd98c6992442a62c7ac7f4
piseganesh123 <piseganesh123@gmail.com>
2019-01-14 11:49:17 +0530

    Test commit

--- kafkaconsumer.py
+++ kafkaconsumer.py
@@ -1,8 +1,8 @@
 from kafka import KafkaConsumer
 consumer = KafkaConsumer('test', bootstrap_servers=['localhost:9092'])
-#for message in consumer:
-#  print ("%s:%d:%d: key=%s value=%s" % (message.topic, message.partition, message.offset, message.key, message.value))
+for message in consumer:
+  print ("%s:%d:%d: key=%s value=%s" % (message.topic, message.partition, message.offset, message.key, message.value))
 
-print (consumer.count())
+#print (consumer.count())
 consumer.commitAsync()
 consumer.close()
