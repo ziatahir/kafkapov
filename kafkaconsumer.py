da03bec4242d1640aeeb7def5b4f89b19eb3e841
piseganesh123 <piseganesh123@gmail.com>
2019-01-14 13:57:41 +0530

    Updated spacing

--- kafkaconsumer.py
+++ kafkaconsumer.py
@@ -6,10 +6,10 @@ counter = 0
 for message in consumer:
     counter = counter + 1
 #    if counter == 16:
-       print ("%s:%d:%d: key=%s value=%s" % (message.topic, message.partition, message.offset, message.key, message.value))
-       if message.value == 'Create_Charm':
-            print ("Call SAP to Create Charm")
-       break
+    print ("%s:%d:%d: key=%s value=%s" % (message.topic, message.partition, message.offset, message.key, message.value))
+    if message.value == 'Create_Charm':
+        print ("Call SAP to Create Charm")
+    break
 
 print (counter)
 consumer.close()
