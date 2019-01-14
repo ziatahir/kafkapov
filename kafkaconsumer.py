3e8685bf721e29684afb0515f5eda5d4fa56ed22
piseganesh123 <piseganesh123@gmail.com>
2019-01-14 12:20:41 +0530

    Mock Call to SAP for creating charm

--- kafkaconsumer.py
+++ kafkaconsumer.py
@@ -1,8 +1,6 @@
 from kafka import KafkaConsumer
 consumer = KafkaConsumer('test', bootstrap_servers=['localhost:9092'])
 
-#  print ("%s:%d:%d: key=%s value=%s" % (message.topic, message.partition, message.offset, message.key, message.value))
-
 counter = 0
 for message in consumer:
     counter = counter + 1
@@ -10,8 +8,7 @@ for message in consumer:
        print ("%s:%d:%d: key=%s value=%s" % (message.topic, message.partition, message.offset, message.key, message.value))
        break
 
-#return counter 
 print (counter)
-
-#consumer.commitAsync()
+if message.value == "Create Charm"
+    print ("Call SAP to Create Charm")
 consumer.close()
