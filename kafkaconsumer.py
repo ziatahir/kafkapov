ccb2c9cf4a4feaebb5e119be9f2afc40061a53cc
piseganesh123 <piseganesh123@gmail.com>
2019-01-14 12:12:32 +0530

    Test message print in Jenkins log

--- kafkaconsumer.py
+++ kafkaconsumer.py
@@ -1,13 +1,13 @@
 from kafka import KafkaConsumer
 consumer = KafkaConsumer('test', bootstrap_servers=['localhost:9092'])
 
-#for message in consumer:
 #  print ("%s:%d:%d: key=%s value=%s" % (message.topic, message.partition, message.offset, message.key, message.value))
 
 counter = 0
 for message in consumer:
     counter = counter + 1
-    if counter == 2:
+    if counter == 1:
+       print ("%s:%d:%d: key=%s value=%s" % (message.topic, message.partition, message.offset, message.key, message.value))
        break
 
 #return counter 
