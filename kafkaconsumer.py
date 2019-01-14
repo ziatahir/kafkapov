9d21b7bc09406cd5c6be6f20bbf92ab9110ec41f
piseganesh123 <piseganesh123@gmail.com>
2019-01-14 14:05:09 +0530

    using nbr message value as 20

--- kafkaconsumer.py
+++ kafkaconsumer.py
@@ -3,13 +3,14 @@ from kafka import KafkaConsumer
 consumer = KafkaConsumer('test', bootstrap_servers=['localhost:9092'], auto_offset_reset='earliest', enable_auto_commit=False, group_id=None)
 
 counter = 0
+def nbr_msg=20:
 for message in consumer:
     counter = counter + 1
 #    if counter == 16:
     print ("%s:%d:%d: key=%s value=%s" % (message.topic, message.partition, message.offset, message.key, message.value))
     if message.value == 'Create_Charm':
         print ("Call SAP to Create Charm")
-    if counter == 20:
+    if counter == nbr_msg:
         break    
 print (counter)
 consumer.close()
