ca7550a16218b185896dfe21660c0d2dc87d7a3d
Tahir <zia.tahir@accenture.com>
2019-02-06 12:37:56 +0530

    updated

--- kafkaconsumer.py
+++ kafkaconsumer.py
@@ -7,7 +7,7 @@ for message in consumer:
     print ("%s:%d:%d: key=%s value=%s" % (message.topic, message.partition, message.offset, message.key, message.value))
     if message.value == 'CharmID-12345':
         print ("Call SAP to Create Charm")
-		break
+    break
 consumer.close()
 
 
