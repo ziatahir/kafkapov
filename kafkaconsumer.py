f7d4a74385a560d5114c36afea704b51aa646a79
Tahir <zia.tahir@accenture.com>
2019-02-06 12:22:42 +0530

    updated

--- kafkaconsumer.py
+++ kafkaconsumer.py
@@ -9,11 +9,6 @@ for message in consumer:
 consumer.close()
 
 
-consumer = KafkaConsumer('topic_TrID', bootstrap_servers=['localhost:9092'], auto_offset_reset='earliest', enable_auto_commit=False, group_id=None)
-for message in consumer:
-    print ("%s:%d:%d: key=%s value=%s" % (message.topic, message.partition, message.offset, message.key, message.value))
-    if message.value == 'TRID-12345':
-        print ("Call SAP to Create Transport")
-consumer.close()
+
 
 
