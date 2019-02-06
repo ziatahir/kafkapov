ef8dbec625805bd76a28ece3f2e62ef906dbaabb
Tahir <zia.tahir@accenture.com>
2019-02-06 11:48:59 +0530

    updated

--- kafkaconsumer - Copy.py
+++ kafkaconsumer - Copy.py
@@ -1,6 +1,6 @@
 from kafka import KafkaConsumer
 #KafkaConsumer(auto_offset_reset='earliest', enable_auto_commit=False, group_id=None)
-consumer = KafkaConsumer('topic_charmID', bootstrap_servers=['localhost:9092'], auto_offset_reset='earliest', enable_auto_commit=False, group_id=None)
+consumer = KafkaConsumer('test', bootstrap_servers=['localhost:9092'], auto_offset_reset='earliest', enable_auto_commit=False, group_id=None)
 
 counter = 0
 nbr_msg = 2
@@ -8,7 +8,7 @@ for message in consumer:
     counter = counter + 1
 #    if counter == 16:
     print ("%s:%d:%d: key=%s value=%s" % (message.topic, message.partition, message.offset, message.key, message.value))
-    if message.value == 'CharmID-12345':
+    if message.value == 'Create_Charm':
         print ("Call SAP to Create Charm")
     if counter == nbr_msg:
         break    
