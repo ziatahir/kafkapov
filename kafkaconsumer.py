7e26841f2b76a8736c9cf9ba4972dc9c2862ebef
Tahir <zia.tahir@accenture.com>
2019-02-06 12:18:33 +0530

    updated

--- kafkaconsumer.py
+++ kafkaconsumer.py
@@ -1,7 +1,8 @@
 from kafka import KafkaConsumer
 #KafkaConsumer(auto_offset_reset='earliest', enable_auto_commit=False, group_id=None)
-consumer = KafkaConsumer('topic_charmID', bootstrap_servers=['localhost:9092'], auto_offset_reset='earliest', enable_auto_commit=False, group_id=None)
 
+consumer = KafkaConsumer('topic_charmID', bootstrap_servers=['localhost:9092'], auto_offset_reset='earliest', enable_auto_commit=False, group_id=None)
+for message in consumer:
 print ("%s:%d:%d: key=%s value=%s" % (message.topic, message.partition, message.offset, message.key, message.value))
 if message.value == 'CharmID-12345':
 print ("Call SAP to Create Charm")
@@ -9,7 +10,10 @@ consumer.close()
 
 
 consumer = KafkaConsumer('topic_TrID', bootstrap_servers=['localhost:9092'], auto_offset_reset='earliest', enable_auto_commit=False, group_id=None)
+for message in consumer:
 print ("%s:%d:%d: key=%s value=%s" % (message.topic, message.partition, message.offset, message.key, message.value))
 if message.value == 'TRID-12345':
 print ("Call SAP to Create Transport")
 consumer.close()
+
+
