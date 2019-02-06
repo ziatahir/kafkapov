81adab879f20d46be7fc58d04338fb073764e88e
Tahir <zia.tahir@accenture.com>
2019-02-06 12:11:19 +0530

    updated

--- kafkaproducer.py
+++ kafkaproducer.py
@@ -2,3 +2,5 @@ from kafka import KafkaProducer
 producer = KafkaProducer(bootstrap_servers='localhost:9092')
 producer.send('topic_charmID', b'CharmID-12345')
 producer.flush()
+producer.send('topic_TrID', b'TRID-12345')
+producer.flush()
