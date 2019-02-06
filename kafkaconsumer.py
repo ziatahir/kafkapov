44d10a1d8e5c1b8188392547e0d10ab93c6bb341
Tahir <zia.tahir@accenture.com>
2019-02-06 12:20:24 +0530

    updated

--- kafkaconsumer.py
+++ kafkaconsumer.py
@@ -3,17 +3,17 @@ from kafka import KafkaConsumer
 
 consumer = KafkaConsumer('topic_charmID', bootstrap_servers=['localhost:9092'], auto_offset_reset='earliest', enable_auto_commit=False, group_id=None)
 for message in consumer:
-print ("%s:%d:%d: key=%s value=%s" % (message.topic, message.partition, message.offset, message.key, message.value))
-if message.value == 'CharmID-12345':
-print ("Call SAP to Create Charm")
+    print ("%s:%d:%d: key=%s value=%s" % (message.topic, message.partition, message.offset, message.key, message.value))
+    if message.value == 'CharmID-12345':
+        print ("Call SAP to Create Charm")
 consumer.close()
 
 
 consumer = KafkaConsumer('topic_TrID', bootstrap_servers=['localhost:9092'], auto_offset_reset='earliest', enable_auto_commit=False, group_id=None)
 for message in consumer:
-print ("%s:%d:%d: key=%s value=%s" % (message.topic, message.partition, message.offset, message.key, message.value))
-if message.value == 'TRID-12345':
-print ("Call SAP to Create Transport")
+    print ("%s:%d:%d: key=%s value=%s" % (message.topic, message.partition, message.offset, message.key, message.value))
+    if message.value == 'TRID-12345':
+        print ("Call SAP to Create Transport")
 consumer.close()
 
 
