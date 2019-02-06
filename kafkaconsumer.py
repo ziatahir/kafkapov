429cf0394a43d8adb0a42b0a63b942528de2c008
Tahir <zia.tahir@accenture.com>
2019-02-06 12:26:43 +0530

    updated

--- kafkaconsumer.py
+++ kafkaconsumer.py
@@ -2,6 +2,7 @@ from kafka import KafkaConsumer
 #KafkaConsumer(auto_offset_reset='earliest', enable_auto_commit=False, group_id=None)
 
 consumer = KafkaConsumer('topic_charmID', bootstrap_servers=['localhost:9092'], auto_offset_reset='earliest', enable_auto_commit=False, group_id=None)
+
 for message in consumer:
     print ("%s:%d:%d: key=%s value=%s" % (message.topic, message.partition, message.offset, message.key, message.value))
     if message.value == 'CharmID-12345':
